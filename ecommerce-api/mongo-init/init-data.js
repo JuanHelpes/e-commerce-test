71f53a09358bd0b8fa37e1c10576d0221a1c00f2
Juan <helpeshs@gmail.com>
2025-08-12 16:39:03 -0300

    feat: docker configs

---
+++ ecommerce-api/mongo-init/init-data.js
@@ -0,0 +1,199 @@
+db = db.getSiblingDB('eCommerce');
+
+db.Product.insertMany([
+  {
+    name: 'Mouse Gamer Redragon Cobra, Chroma RGB, 12400 DPI, 8 Botões, Preto',
+    code_product: '6',
+    description:
+      'Prepare-se para elevar sua experiência de jogo a um novo patamar com o mouse gamer definitivo. Equipado com o sensor de alta performance PIXART PMW3327 (20 G / 100 IPS), este mouse oferece precisão cirúrgica e movimentos ultra-rápidos, garantindo que cada clique e movimento seja executado com perfeição.',
+    price: 89.9,
+    url_image:
+      'https://images5.kabum.com.br/produtos/fotos/94555/mouse-gamer-redragon-cobra-chroma-rgb-10000dpi-7-botoes-preto-m711-v2_1742821619_gg.jpg',
+    createdAt: {
+      $date: '2025-08-05T14:53:46.953Z',
+    },
+    updatedAt: {
+      $date: '2025-08-05T14:53:46.953Z',
+    },
+    userId: null,
+  },
+  {
+    name: 'Headset Gamer Havit, Drivers 53mm, Microfone Plugável, 3.5mm, PC, PS4, XBOX ONE, Preto',
+    code_product: '6',
+    description:
+      'O Headset Gamer Havit H2002D combina uma estética moderna e funcional com materiais de alta qualidade. Sua aparência proporcional e design gamer em preto, com detalhes em liga leve e acabamento em plástico com revestimento de piano, garantem um visual elegante que complementa qualquer setup. Mas a beleza é apenas o começo: este headset oferece um desempenho excepcional e compatibilidade versátil com PC, PS4 e Xbox, tudo isso com um preço que surpreende pela qualidade entregue.',
+    price: 168.1,
+    url_image:
+      'https://images0.kabum.com.br/produtos/fotos/102770/headset-gamer-havit-drivers-53mm-hv-h2002d_headset-gamer-havit-drivers-53mm-hv-h2002d_1564056874_gg.jpg',
+    createdAt: {
+      $date: '2025-08-05T14:53:46.953Z',
+    },
+    updatedAt: {
+      $date: '2025-08-05T14:53:46.953Z',
+    },
+    userId: null,
+  },
+  {
+    name: 'Notebook Lenovo IdeaPad 1i Intel Core I3-1215U, 4GB RAM, SSD 256GB, Tela 15.6',
+    code_product: '5',
+    description:
+      'O IdeaPad 1i eleva sua categoria de notebooks com um processador Intel super eficiente de 12ª geração em um chassi fino e compacto de 17,9 mm que facilita a multitarefa enquanto aumenta a eficiência energética com até 11 horas de duração da bateria e carga rápida. Obtenha aproveitamento máximo da tela com as molduras super estreitas, alto-falantes Dolby Audio e Flip to Start para inicialização instantânea. Otimize sua experiência de chamada de vídeo com uma câmera de 1MP que vem com um obturador de privacidade para afastar os olhos curiosos e o Cancelamento de Ruído Inteligente para cortar o ruído de fundo no seu microfone.',
+    price: 2499.9,
+    url_image:
+      'https://images8.kabum.com.br/produtos/fotos/522818/notebook-gamer-lenovo-intel-core-i3-1215u-4gb-ram-ssd-256gb-tela-15-6-hd-windows-11-cinza-82vy000tbr_1709576362_gg.jpg',
+    createdAt: {
+      $date: '2025-08-05T14:53:46.953Z',
+    },
+    updatedAt: {
+      $date: '2025-08-05T14:53:46.953Z',
+    },
+    userId: null,
+  },
+  {
+    name: 'Notebook Gamer Acer Nitro V, Intel Core i7-13ªGen, 16GB, SSD 512GB, RTX4050, Tela 15.6 Full HD',
+    code_product: '5',
+    description:
+      'Processador: Intel® Core™ I7 Sist. Operacional: Linux Gutta Memória: 16 Gb Armazenamento: Ssd- 512 Gb Modelo da Placa de Vídeo: Nvidia Rtx 4050 Com 6 Gb Tamanho da Tela: 15.6 Polegadas Placa de Vídeo: Dedicada Taxa de Atualização: 144 Hz Resolução da Tela: Full Hd - 1920 x 1080',
+    price: 5750.99,
+    url_image:
+      'https://images7.kabum.com.br/produtos/fotos/sync_mirakl/615577/xlarge/Notebook-Gamer-Acer-Nitro-V-Intel-Core-i7-13-Gen-16GB-SSD-512GB-RTX4050-Tela-15-6-Full-HD-Linux-Gutta-Anv15-51-7037_1751663078.jpg',
+    createdAt: {
+      $date: '2025-08-05T14:53:46.953Z',
+    },
+    updatedAt: {
+      $date: '2025-08-05T14:53:46.953Z',
+    },
+    userId: null,
+  },
+  {
+    name: 'Placa-Mãe ASUS TUF GAMING A520M-PLUS II, AMD AM4, mATX, DDR4, Preto',
+    code_product: '4',
+    description:
+      'A Placa Mãe Asus TUF GAMING A520M-PLUS II, foi projetada com os mais altos padrões de qualidade e tecnologia TUF LANGuard e Turbo LAN. Criada especialmente para jogos online, surpreende até os gamers mais exigentes. Compatível com processadores Ryzen 5000 Series/4000 G-Series e praticamente toda a linha 3000 Series de Desktop Processors, oferece tecnologia de resfriamento abrangente graças ao trio: dissipador de calor VRM, dissipador de calor PCH, Fan Xpert 2+. para melhor resfriamento e maior desempenho dos seus componentes do seu PC Gamer.',
+    price: 575.99,
+    url_image:
+      'https://images3.kabum.com.br/produtos/fotos/165133/placa-mae-asus-tuf-gaming-a520m-plus-ii-amd-am4-matx-ddr4-preto-90mb17g0-m0eay0_1736968906_gg.jpg',
+    createdAt: {
+      $date: '2025-08-05T14:53:46.953Z',
+    },
+    updatedAt: {
+      $date: '2025-08-05T14:53:46.953Z',
+    },
+    userId: null,
+  },
+  {
+    name: 'Placa de Vídeo RTX 3060 Ventus 2X 12G OC MSI GeForce, 12GB GDDR6, 15 Gbps, Ray Tracing',
+    code_product: '4',
+    description:
+      'VENTUS tem um design focalizado no desempenho que mantém o essencial para cumprir com qualquer tarefa que você tem em mãos. Uma boa distribuição do ventilador duplo no design industrial rígido faz com que esta placa de vídeo elegante combine com qualquer sistema.',
+    price: 1999.99,
+    url_image:
+      'https://images4.kabum.com.br/produtos/fotos/153454/placa-de-video-msi-geforce-rtx-3060-ventus-2x-12g-oc-15-gbps-12gb-gddr6-ray-tracing_1616505819_gg.jpg',
+    createdAt: {
+      $date: '2025-08-05T14:53:46.953Z',
+    },
+    updatedAt: {
+      $date: '2025-08-05T14:53:46.953Z',
+    },
+    userId: null,
+  },
+  {
+    name: 'Console Nintendo Switch 2 + Jogo Digital Mario Kart World',
+    code_product: '2',
+    description:
+      'Comece sua aventuras no Nintendo Switch 2 com este bundle que inclui um console e o download do jogo Mario Kart World completo, exclusivo para o Nintendo Switch 2. O Nintendo Switch 2 é a próxima evolução do console Nintendo Switch — repleto de melhorias e maneiras de se conectar e jogar com outros!',
+    price: 4450.99,
+    url_image:
+      'https://images8.kabum.com.br/produtos/fotos/779788/console-nintendo-switch-2-mario-kart-world_1745844335_gg.jpg',
+    createdAt: {
+      $date: '2025-08-05T14:53:46.953Z',
+    },
+    updatedAt: {
+      $date: '2025-08-05T14:53:46.953Z',
+    },
+    userId: null,
+  },
+  {
+    name: 'Controle Sony DualSense PS5, Sem fio, Edição Limitada Do 30º Aniversário, Cinza e Preto',
+    code_product: '2',
+    description:
+      'Comemore os 30 anos do PlayStation com a Edição Limitada do Controle Sem Fio DualSense. Este controle especial possui design retrô e um esquema de cores que homenageia o lendário console original, lançado em 3 de dezembro de 1994.',
+    price: 599.9,
+    url_image:
+      'https://images5.kabum.com.br/produtos/fotos/640515/controle-sony-dualsense-sem-ps5-sem-fio-edicao-limitada-do-30-aniversario-cinza-e-preto-1000046932_1727189692_gg.jpg',
+    createdAt: {
+      $date: '2025-08-05T14:53:46.953Z',
+    },
+    updatedAt: {
+      $date: '2025-08-05T14:53:46.953Z',
+    },
+    userId: null,
+  },
+  {
+    name: 'Cadeira Gamer Fox Office Fox Racer, RGB e Iluminação LED, até 130kgs, com almofadas, Reclinável - Preta',
+    code_product: '1',
+    description:
+      'A Cadeira RGB DA FOX foi desenhada com faixa de LED RGB em seu contorno, a cores da faixa de LED RGB são configuráveis via controle remoto, podendo deixar na cor de sua preferência (azul, vermelha, amarela, verde, violeta, pink) ou até mesmo fazer um mix de cores, o Led é alimentado com Power Bank através de entrada USB.',
+    price: 659,
+    url_image:
+      'https://images3.kabum.com.br/produtos/fotos/sync_mirakl/150593/xlarge/Cadeira-Gamer-Fox-Office-Fox-Racer-RGB-e-Ilumina-o-LED-at-130kgs-com-almofadas-Reclin-vel-Preta_1740600041.jpg',
+    createdAt: {
+      $date: '2025-08-05T14:53:46.953Z',
+    },
+    updatedAt: {
+      $date: '2025-08-05T14:53:46.953Z',
+    },
+    userId: null,
+  },
+  {
+    name: 'Cadeira Gamer Fortt Trieste – Branca, até 120 kg, Reclinável, com Apoio de Pés (CGF002‑B)',
+    code_product: '1',
+    description:
+      'A Cadeira Gamer Fortt Trieste CGF002‑B oferece conforto e estilo para longas jornadas de jogos, estudos ou trabalho. Seu encosto reclinável de até 135° com apoio de pés integrado traz relaxamento total. A base giratória de 360° e ajuste de altura adaptam a cadeira ao seu biotipo, enquanto o revestimento em couro PU com travesseiros para lombar e cabeça garante ergonomia. Suporta até 120 kg e é ideal para setups modernos.',
+    price: 550,
+    url_image:
+      'https://images2.kabum.com.br/produtos/fotos/sync_mirakl/594812/Cadeira-Gamer-Fortt-Trieste-At-120KG-Reclin-vel-Branca-Cgf002-b_1720618903_gg.jpg',
+    createdAt: {
+      $date: '2025-08-05T14:53:46.953Z',
+    },
+    updatedAt: {
+      $date: '2025-08-05T14:53:46.953Z',
+    },
+    userId: null,
+  },
+  {
+    name: 'PC Gamer Skill Aquarium – Ryzen 5 5600GT, RTX 3060 12GB, 16GB RAM, SSD 1TB M.2, Fonte 600W 80 Plus, Branco (SKA072)',
+    code_product: '3',
+    description:
+      'Desfrute de potência e estilo com o PC Gamer Skill Aquarium SKA072. Equipada com o processador AMD Ryzen 5 5600GT e a placa de vídeo NVIDIA GeForce RTX 3060 com 12 GB de memória, este setup oferece performance consistente em títulos AAA. Com 16 GB de RAM DDR4 a 3200 MHz e SSD M.2 de 1 TB, você tem rapidez no carregamento de jogos e espaço de sobra para suas bibliotecas. A fonte de 600W com certificação 80 Plus garante energia estável mesmo nas sessões mais exigentes. O gabinete branco estilo aquarium com três fans RGB inclusos entrega visual moderno e ventilação eficiente.',
+    price: 4895.1,
+    url_image:
+      'https://images0.kabum.com.br/produtos/fotos/sync_mirakl/705190/PC-Gamer-Skill-Aquarium-Amd-Ryzen-5-5600gt-16gb-3200mhz-Geforce-RTX-3060-12gb-SSD-1TB-M-2-Fonte-600w-80-Plus-Branco-Ska072_1738286803_gg.jpg',
+    url_image_2:
+      'https://images0.kabum.com.br/produtos/fotos/sync_mirakl/705190/PC-Gamer-Skill-Aquarium-Amd-Ryzen-5-5600gt-16gb-3200mhz-Geforce-RTX-3060-12gb-SSD-1TB-M-2-Fonte-600w-80-Plus-Branco-Ska072_1738286814_gg.jpg',
+    createdAt: {
+      $date: '2025-08-05T14:53:46.953Z',
+    },
+    updatedAt: {
+      $date: '2025-08-05T14:53:46.953Z',
+    },
+    userId: null,
+  },
+  {
+    name: 'PC Gamer ITX Arena Setup Powered By Asus, I5 10400F, Asus GTX 1650 4GB, 16GB, SSD 480GB',
+    code_product: '3',
+    description:
+      'Monte o setup dos seus sonhos com o PC Gamer ITX Arena Setup Powered By Asus. Equipado com o poderoso processador Intel Core i5 10400F e a placa de vídeo Asus GTX 1650 4GB, este computador é ideal para quem busca desempenho em jogos e multitarefas com ótimo custo-benefício. Com 16GB de memória RAM e um SSD de 480GB, você garante inicializações rápidas, carregamento veloz de jogos e fluidez em aplicações do dia a dia. O design compacto no formato ITX combina performance e estilo, perfeito para setups minimalistas sem abrir mão da potência. Ideal para títulos como Valorant, Fortnite, GTA V e muitos outros.',
+    price: 4189,
+    url_image: 'https://m.media-amazon.com/images/I/61KdJ51oAfL._AC_SX679_.jpg',
+    url_image_2:
+      'https://m.media-amazon.com/images/I/61IGnIHvInL._AC_SX679_.jpg',
+    createdAt: {
+      $date: '2025-08-05T14:53:46.953Z',
+    },
+    updatedAt: {
+      $date: '2025-08-05T14:53:46.953Z',
+    },
+    userId: null,
+  },
+]);
